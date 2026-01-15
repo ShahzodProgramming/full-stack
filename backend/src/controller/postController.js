4d10487e0b9e5118394477e8b0e454c3fce7a615
ShahzodProgramming <bebap81246@gmail.com>
2026-01-15 20:52:33 +0500

    Edit posts

--- backend/src/controller/postController.js
+++ backend/src/controller/postController.js
@@ -44,3 +44,45 @@ export const postGet = async (req, res) => {
       .json({ message: "Something went wrong in the server" });
   }
 };
+
+export const postEdit = async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    const { authorization } = req.headers;
+    const { postId } = req.params;
+    const exists = await postModal.findById(postId);
+
+    if (!exists) {
+      console.log("Failed to find the post");
+      return res.status(404).json({ message: "Failed to find the post" });
+    }
+    const userIdFromToken = await jwt.decode(authorization.split(" ")[1]).id;
+    console.log(exists.userId, userIdFromToken);
+    if (exists.userId !== userIdFromToken) {
+      console.log("User trying to change the post of another user");
+      return res.status(403).json({
+        message: "Not allowed, can't change the post of another person",
+      });
+    }
+
+    const editedPost = await postModal.findByIdAndUpdate(
+      postId,
+      { title, content },
+      { new: true }
+    );
+
+    if (!editedPost) {
+      console.log("Failed to edit the post");
+      return res.status(404).json({ message: "Failed to edit the post" });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Post edited successfully", post: editedPost });
+  } catch (error) {
+    console.error("An error occured", error);
+    return res
+      .status(500)
+      .json({ message: "Something went wrong in the server" });
+  }
+};
--- backend/src/router/postRouter.js
+++ backend/src/router/postRouter.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { checkAuth } from "../middleware/authCheck.js";
-import { postCreate, postGet } from "../controller/postController.js";
+import { postCreate, postEdit, postGet } from "../controller/postController.js";
 
 const postRouter = express.Router();
 
 postRouter.post("/create", checkAuth, postCreate);
 postRouter.post("/get", checkAuth, postGet);
+postRouter.put("/edit/:postId", checkAuth, postEdit);
 
-// postRouter.post("/delete:postId");
-// postRouter.post("/edit:postId");
+// postRouter.delete("/delete:postId");
 
 export default postRouter;
